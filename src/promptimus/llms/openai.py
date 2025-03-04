ff79da79aad989733791f6373097de8ad58ff6a7
ailadin <korzhukandrew@gmail.com>
2025-03-04 09:56:49 +0200

    allowed to pass call kwargs

--- src/promptimus/llms/openai.py
+++ src/promptimus/llms/openai.py
@@ -4,14 +4,21 @@ from promptimus.dto import History, Message, MessageRole
 
 
 class OpenAILike:
-    def __init__(self, model_name: str, **client_kwargs):
+    def __init__(
+        self,
+        model_name: str,
+        call_kwargs: dict | None = None,
+        **client_kwargs,
+    ):
         self.client = AsyncOpenAI(**client_kwargs)
         self.model_name = model_name
+        self.call_kwargs = {} if call_kwargs is None else call_kwargs
 
     async def achat(self, history: list[Message]) -> Message:
         response = await self.client.chat.completions.create(
             messages=History.dump_python(history),
             model=self.model_name,
+            **self.call_kwargs,
         )
 
         assert response.choices, response
